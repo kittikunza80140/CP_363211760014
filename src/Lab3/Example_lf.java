852dce76b9353ef64b34e378bf3bd9cef7ca121c
captain <kittikhun.s@rmutsvmail.vom>
2020-08-11 18:49:09 +0700

    Lab3

---
+++ src/Lab3/Example_lf.java
@@ -0,0 +1,19 @@
+package Lab3;
+
+public class Example_lf {
+
+    public static  void main(String[] args){
+
+    int x,y;
+
+    x = 10;
+    y = 20;
+
+    if (x>y){
+        //.....statement of if
+        System.out.println("x grater than y.");
+    }
+        System.out.println("Good Bye");
+
+    }
+}
